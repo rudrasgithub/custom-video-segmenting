8aa15cc5c49706ba48cec463afffe7c1e26b0672
rudrasgithub <rudramanaidu99@gmail.com>
2024-08-03 11:42:22 +0530

    updated code

--- frontend/@latest/src/components/DownloadUrl.jsx
+++ frontend/@latest/src/components/DownloadUrl.jsx
@@ -1,12 +1,14 @@
 import {useEffect, useState} from 'react'
 import axios from 'axios'
 import {BACKEND_URL} from "../../config";
+import ReactPlayer from 'react-player';
 
 export default function DownloadUrl() {
     const [url,setUrl]=useState("");
     const [video,setVideo]=useState(null);
     const [error,setError]=useState(null)
     const [loading,setLoading]=useState(false);
+    const [getStream, setGetStream] = useState(false);
 
     const getQualityValue = (qualityLabel) => {
         const match = qualityLabel.match(/(\d+)p/);
@@ -14,14 +16,27 @@ export default function DownloadUrl() {
     };
     const mp4Formats = (
         video?.availableFormats
-        .filter(format => format.hasVideo && format.mimeType.includes('mp4')) // MP4 files with video
-        .filter(format => getQualityValue(format.quality) >= 240) // Quality filter
+        .filter(format => format.hasVideo && format.mimeType.includes('mp4'))
         .sort((a, b) => getQualityValue(a.quality) - getQualityValue(b.quality))
-    ); 
+        .reduce((map,format) => {
+            const quality = format.quality;
+            const hasAudio = format.hasAudio;
+            const { itag } = format.itag;
+
+            if (!map.has(quality)) {
+                map.set(quality, [{hasAudio, itag}]);
+            } else {
+                const existingValues = map.get(quality);
+                if (hasAudio===true && existingValues.hasAudio!==true){
+                    map.set(quality, [...existingValues, {hasAudio, itag}]);
+                }
+            }
+            return map;
+        }, new Map()));
+
     const webmFormats = (
         video?.availableFormats
         .filter(format => format.hasVideo && format.mimeType.includes('webm')) // WebM files with video
-        .filter(format => getQualityValue(format.quality) >= 240) // Quality filter
         .sort((a, b) => getQualityValue(a.quality) - getQualityValue(b.quality))
     );
     const downloadVideo = async (itag, outputPath) => {
@@ -61,7 +76,8 @@ export default function DownloadUrl() {
             }
         }
         handleSubmit();
-    }, [url])
+    }, [url]);
+
     return (
         <div className='bg-gray-200 min-h-screen min-w-full'>
             <div className='flex flex-col items-center min-w-full'>
@@ -75,21 +91,38 @@ export default function DownloadUrl() {
                         value={url}
                         onChange={(e)=>setUrl(e.target.value)}
                     />
-                    {loading?<span className='text-green-500 text-xl font-bold'>Processing...</span>:''}
-                    {/* {error?<span>Please provide valid url</span>:''} */}
+                    {error?<span>Please provide valid url</span>:(
+                        loading?<span className='text-green-500 text-xl font-bold'>Processing...</span>:''
+                    )}
                 </div>
             </div>
-            <div className='flex h-max'>
                 {video && (
-                    <div className='grid grid-cols-2 gap-5'>
-                        <div className='ml-28 mr-10 flex flex-col justify-center items-center'>
-                            <img 
-                                src={video.thumbnail.url}
-                                height={video.thumbnail.height} 
-                                width={video.thumbnail.width} 
-                                className='rounded-sm'
-                            />
-                            <h2 className='mt-5 font-semibold text'>{video.title}</h2>
+                    <div className='grid grid-cols-2 gap-16'>
+                        <div className='ml-28 mr-10 flex flex-col items-end justify-center'>
+                            {!getStream ? (
+                                <img 
+                                    src={video.thumbnail.url}
+                                    width='400'
+                                    className='rounded-sm cursor-pointer'
+                                    onClick={() => setGetStream(true)}
+                                />
+                            ) : (
+                                <div className='bg-black'>
+                                    <ReactPlayer
+                                        url={video.url}
+                                        controls
+                                        width='100%'
+                                        height='auto'
+                                    />
+                                    <button
+                                        onClick={() => setGetStream(false)}
+                                        className='absolute top-5 right-5 bg-red-500 text-white p-2 rounded'
+                                    >
+                                        Close
+                                    </button>
+                                    </div>
+                            )}
+                            <h2 className='mt-5 font-semibold text'>{video.title.substring(0,50)}</h2>
                             <span className='font-semibold'>Duration: {video.TotalVideoDuration}</span>
                         </div>
                         <div>
@@ -97,24 +130,26 @@ export default function DownloadUrl() {
                             <div className='flex'>
                                 <div>
                                     <h3 className='pl-20 mt-10 mr-20 text-lg font-semibold mb-4'>MP4 Files</h3>
-                                    {mp4Formats
-                                        .map(format => (
-                                            <div 
-                                                key={format.itag}
-                                                className='flex mb-4'
-                                            >
-                                                <button
-                                                    className='flex justify-center items-center px-4 py-2 w-[150px] h-[50px] bg-green-500 text-white rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-blue-400'
-                                                    onClick={()=>downloadVideo(format.itag, `video_${format.quality}.mp4`)}
+                                    {Array.from(mp4Formats.entries()).map(([quality, formats]) => (
+                                        <div key={quality} className='flex flex-col'>
+                                            {formats.map(({ hasAudio, itag }) => (
+                                                <div 
+                                                    key={itag}
+                                                    className='flex mb-4'
                                                 >
-                                                    {format.quality}
-                                                </button>
-                                                {!format.hasAudio && (
-                                                    <span className='flex justify-center items-center'>🔇</span>
-                                                )}
-                                            </div>
-                                        ))
-                                    }
+                                                    <button
+                                                        className='flex justify-center items-center px-4 py-2 w-[150px] h-[50px] bg-green-500 text-white rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-blue-400'
+                                                        onClick={()=>downloadVideo(itag, `video_${quality}.mp4`)}
+                                                    >
+                                                        {quality}
+                                                    </button>
+                                                    {!hasAudio && (
+                                                        <span className='flex justify-center items-center'>🔇</span>
+                                                    )}
+                                                </div>
+                                            ))}
+                                        </div>
+                                    ))}
                                 </div>
                                 {webmFormats.length > 0 && (
                                     <div>
@@ -140,7 +175,6 @@ export default function DownloadUrl() {
                         </div>
                     </div>
                 )}
-            </div>
         </div>
     )
 }
